6230f4bec4a0391359842dc7c3380f00e73fb669
MinhaMuhammad <minhaghulammuhammad@gmail.com>
2026-09-07 21:55:50 +0500

    changes implemented

--- src/components/data/contactInfo.js
+++ src/components/data/contactInfo.js
@@ -4,8 +4,6 @@ export const CONTACT = {
   email: 'Mapleleafmontessori11@gmail.com',
   instagram: 'https://www.instagram.com/mapleleafmontessori2026/?hl=en#',
   facebook: 'https://www.facebook.com/profile.php?id=61593514284351',
-  pinterest: 'https://www.pinterest.com/',
-  youtube: 'https://www.youtube.com/',
   hours: 'Mon – Fri: 10:00 AM – 5:00 PM',
   hoursShort: '10:00 AM – 5:00 PM',
 };
